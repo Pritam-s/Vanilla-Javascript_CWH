fb43c9f9fe92c58bc4925d972e3c3360d43f8f78
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2021-09-07 00:23:24 +0530

    Full chapter.

--- NodeJs/5.NodeJs_Creating_a_custom_backend_Using_NodeJs/index.js
+++ NodeJs/5.NodeJs_Creating_a_custom_backend_Using_NodeJs/index.js
@@ -7,13 +7,33 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const home = fs.readFileSync('index.html');
 const about = fs.readFileSync('about.html');
-const 
+const contact = fs.readFileSync('contact.html');
+const services = fs.readFileSync('services.html');
 
 // Now we will create a server and give it an arrow function 
 const server = http.createServer((req, res) => {
+    console.log(req.url)
+    url = req.url;
+
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end(home);
+
+    if (url == '/') {
+      res.end(home);
+    }
+    else if (url == '/about.html'){
+      res.end(about);
+    }
+    else if (url == '/contact.html'){
+      res.end(contact);
+    }
+    else if (url == '/services.html'){
+      res.end(services);
+    }
+    else{
+      res.statusCode = 404;
+      res.end("<h1>404 not found</h1>");
+  }
   });
 
 // And finally, we will make the port to listen-
