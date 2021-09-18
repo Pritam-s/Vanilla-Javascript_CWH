b72b66a32028d9050fd4539d8ea46d57f3dbf06b
SaGE1704 <51298487+SaGE1704@users.noreply.github.com>
2021-09-18 18:09:43 +0530

    Getting the output in the terminal as an object.

--- NodeJs/12.NodeJs_Using_Raw_HTML_and_Backend_In_Pug/app.js
+++ NodeJs/12.NodeJs_Using_Raw_HTML_and_Backend_In_Pug/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express();
 const port = 80;
 const path = require('path');
+const fs = require('fs')
 
 // EXPRESS SPECIFIC
 // serving static files
 app.use('/static', express.static('static'));
+app.use(express.urlencoded())
 
 
 // PUG SPECIFIC 
@@ -15,13 +17,21 @@ app.set('views', path.join(__dirname, 'views'));
 
 // ENDPOINTS
 app.get('/', function(req, res){
-    params = {
+    const params = {
         'title' : "Gym Subscription Form",
         'para1' : "Mindblowing offer for all the hardcores gym goers out there."
 
     }
     res.render('index.pug', params);
 });
+
+app.post('/', function(req, res){
+
+    console.log(req.body) // Gets you the output as an object in the terminal
+
+    const params = {'message': 'Your form has been submitted successfully'}
+    res.status(200).render('index.pug', params);
+})
 
 
 // SERVER LISTENING PORT
